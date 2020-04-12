27a8cebb9eee277fc1cf9bbdc4000a493bf7c7b1
Tommy Toban <tommy.deppatori@gmail.com>
2020-04-12 22:24:52 +0700

    init build

---
+++ precache-manifest.ffb397f9a1b8e4853aabb700b5747fc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7837f71e2de4b6255bbb5bbe1322d5ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "326e9ce1eb4c952e753f",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "a11629c342d3db7b1f4c",
+    "url": "/static/css/main.cbdf4783.chunk.css"
+  },
+  {
+    "revision": "326e9ce1eb4c952e753f",
+    "url": "/static/js/2.a451a45f.chunk.js"
+  },
+  {
+    "revision": "82fd2133711d1e027fde858069bc8536",
+    "url": "/static/js/2.a451a45f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a11629c342d3db7b1f4c",
+    "url": "/static/js/main.20cd8022.chunk.js"
+  },
+  {
+    "revision": "90a9eb3501d711818dbf",
+    "url": "/static/js/runtime-main.7eb5af94.js"
+  }
+]);
