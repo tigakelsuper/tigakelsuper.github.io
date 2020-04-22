058df7151ffc8127572aefdb148150b50bb9d05f
Tommy Toban <tigakel.super@gmail.com>
2020-04-23 00:28:07 +0700

    versi 0.2.1

---
+++ precache-manifest.fe6f70fbb7dbc01a3c4d909fad9cc715.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17865a7354ac3b49a27d086414a0dd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86e0d2f167deb176979d",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "dfa98b77db0ce82a5865",
+    "url": "/static/css/main.cbdf4783.chunk.css"
+  },
+  {
+    "revision": "86e0d2f167deb176979d",
+    "url": "/static/js/2.22b7ef63.chunk.js"
+  },
+  {
+    "revision": "82fd2133711d1e027fde858069bc8536",
+    "url": "/static/js/2.22b7ef63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa98b77db0ce82a5865",
+    "url": "/static/js/main.434c470b.chunk.js"
+  },
+  {
+    "revision": "90a9eb3501d711818dbf",
+    "url": "/static/js/runtime-main.7eb5af94.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91bf21579d3e880721957e37c80f3532.js"
+  "/precache-manifest.fe6f70fbb7dbc01a3c4d909fad9cc715.js"
 );
 
 self.addEventListener('message', (event) => {
